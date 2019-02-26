929715e720a3f0ebcf00e04dc6b1266edaf537cd
abuna1985 <adamjabundis@gmail.com>
2019-02-25 20:02:49 -0800

    app/wp-content/themes/wrong-turn-winery-theme/includes/winery-files.php Added reminder in the comments to revert the microtime function back to a version number. This was done to prevent caching.

--- app/wp-content/themes/wrong-turn-winery-theme/includes/winery-files.php
+++ app/wp-content/themes/wrong-turn-winery-theme/includes/winery-files.php
@@ -1,9 +1,11 @@
 <?php
 
 function winery_files() {
+  // TODO: revert microtime() back to version '1.0' before this goes live
   wp_enqueue_script('main-winery-js', get_theme_file_uri('/js/scripts-bundled.js'), NULL, microtime(), true);
   wp_enqueue_style('custom-google-fonts', '//fonts.googleapis.com/css?family=Roboto+Condensed:300,300i,400,400i,700,700i|Roboto:100,300,400,400i,700,700i');
   wp_enqueue_style('font-awesome', '//maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css');
+  // TODO: revert microtime() back to version '1.0' before this goes live
   wp_enqueue_style('winery_main_styles', get_stylesheet_uri(), NULL, microtime());
 }
 
