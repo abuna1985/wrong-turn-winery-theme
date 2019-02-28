ed270ed615aacab638c1b0dcfc3f601754686396
abuna1985 <adamjabundis@gmail.com>
2019-02-28 00:23:49 -0800

    wp-content/themes/wrong-turn-winery-theme/index.php & front-page.php shifted home page to front-page.php and blog page to index.php

--- app/wp-content/themes/wrong-turn-winery-theme/index.php
+++ app/wp-content/themes/wrong-turn-winery-theme/index.php
@@ -1,105 +1,37 @@
-<?php get_header(); ?>
+<?php
 
-  <div class="page-banner">
-  <div class="page-banner__bg-image" style="background-image: url(<?php echo get_theme_file_uri('/images/winery-hero.jpg') ?>);"></div>
-    <div class="page-banner__content container t-center c-white">
-      <h1 class="headline headline--large">Welcome!</h1>
-      <h2 class="headline headline--medium">We think you&rsquo;ll like it here.</h2>
-      <h3 class="headline headline--small">Why don&rsquo;t you check our collection to see what <strong>wine</strong> you&rsquo;re interested in?</h3>
-      <a href="#" class="btn btn--large btn--blue">Find Your Wine</a>
-    </div>
-  </div>
+get_header(); ?>
 
-  <div class="full-width-split group">
-    <div class="full-width-split__one">
-      <div class="full-width-split__inner">
-        <h2 class="headline headline--small-plus t-center">Upcoming Events</h2>
-        
-        <div class="event-summary">
-          <a class="event-summary__date t-center" href="#">
-            <span class="event-summary__month">Mar</span>
-            <span class="event-summary__day">25</span>  
-          </a>
-          <div class="event-summary__content">
-            <h5 class="event-summary__title headline headline--tiny"><a href="#">White Wine Festival</a></h5>
-            <p>Bring you&rsquo;re glasses to enjoy over 40 wines this Saturday for a tasting and snacks. <a href="#" class="nu gray">Learn more</a></p>
-          </div>
-        </div>
-        <div class="event-summary">
-          <a class="event-summary__date t-center" href="#">
-            <span class="event-summary__month">Apr</span>
-            <span class="event-summary__day">02</span>  
-          </a>
-          <div class="event-summary__content">
-            <h5 class="event-summary__title headline headline--tiny"><a href="#">Red Wine Festival</a></h5>
-            <p>Live music, over 30 red wines, and more can found in our third red wine festival. <a href="#" class="nu gray">Learn more</a></p>
-          </div>
-        </div>
-        
-        <p class="t-center no-margin"><a href="#" class="btn btn--blue">View All Events</a></p>
-
-      </div>
+<div class="page-banner">
+  <div class="page-banner__bg-image" style="background-image: url(<?php echo get_theme_file_uri('/images/bottles.jpg') ?>);"></div>
+    <div class="page-banner__content container container--narrow">
+      <h1 class="page-banner__title">Welcome to our blog!</h1>
+      <div class="page-banner__intro">
+        <p>Keep up with our latest news.</p>
     </div>
-    <div class="full-width-split__two">
-      <div class="full-width-split__inner">
-        <h2 class="headline headline--small-plus t-center">From Our Blogs</h2>
-
-        <div class="event-summary">
-          <a class="event-summary__date event-summary__date--beige t-center" href="#">
-            <span class="event-summary__month">Jan</span>
-            <span class="event-summary__day">20</span>  
-          </a>
-          <div class="event-summary__content">
-            <h5 class="event-summary__title headline headline--tiny"><a href="#">We Were Voted Best Tasting Room</a></h5>
-            <p>For the 3rd year in a row we are voted #1. <a href="#" class="nu gray">Read more</a></p>
-          </div>
-        </div>
-        <div class="event-summary">
-          <a class="event-summary__date event-summary__date--beige t-center" href="#">
-            <span class="event-summary__month">Feb</span>
-            <span class="event-summary__day">04</span>  
-          </a>
-          <div class="event-summary__content">
-            <h5 class="event-summary__title headline headline--tiny"><a href="#">Staff in the Regional Spotlight</a></h5>
-            <p>Two of our employees have been in county news lately. <a href="#" class="nu gray">Read more</a></p>
-          </div>
-        </div>
-        
-        <p class="t-center no-margin"><a href="#" class="btn btn--yellow">View All Blog Posts</a></p>
-      </div>
-    </div>
-  </div>
-
-  <div class="hero-slider">
-  <div class="hero-slider__slide" style="background-image: url(<?php echo get_theme_file_uri('/images/shuttle.jpg') ?>);">
-    <div class="hero-slider__interior container">
-      <div class="hero-slider__overlay">
-        <h2 class="headline headline--medium t-center">Free Transportation</h2>
-        <p class="t-center">We have shuttles that pick up and drop off daily.</p>
-        <p class="t-center no-margin"><a href="#" class="btn btn--blue">Learn more</a></p>
-      </div>
-    </div>
-  </div>
-  <div class="hero-slider__slide" style="background-image: url(<?php echo get_theme_file_uri('/images/grapes.jpg') ?>);">
-    <div class="hero-slider__interior container">
-      <div class="hero-slider__overlay">
-        <h2 class="headline headline--medium t-center">An Glass a Day</h2>
-        <p class="t-center">Our sommeliers recommends drinking a glass of red wine a day.</p>
-        <p class="t-center no-margin"><a href="#" class="btn btn--blue">Learn more</a></p>
-      </div>
-    </div>
-  </div>
-  <div class="hero-slider__slide" style="background-image: url(<?php echo get_theme_file_uri('/images/food.jpg') ?>);">
-    <div class="hero-slider__interior container">
-      <div class="hero-slider__overlay">
-        <h2 class="headline headline--medium t-center">Delicious Food Options</h2>
-        <p class="t-center">Wrong Turn Winery offers excellent food options for lunch and dinner.</p>
-        <p class="t-center no-margin"><a href="#" class="btn btn--blue">Learn more</a></p>
-      </div>
-    </div>
-  </div>
+  </div>  
 </div>
 
-  <?php get_footer();
+<div class="container container--narrow page-section">
+<?php 
+  while(have_posts()) {
+   the_post(); ?> 
+   <div class="post-item">
+    <h2 class="headline headline--medium headline--post-title"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
+    <div class="metabox">
+      <p>Posted by <?php the_author_posts_link(); ?> on <?php the_time('n.j.y'); ?> in <?php echo get_the_category_list(', '); ?></p>
+    </div>
+    <div class="generic-content">
+      <?php the_excerpt(); ?>
+      <p><a class="btn btn--blue" href="<?php the_permalink(); ?>">Continue reading &raquo;</a></p>
+    </div>
+   </div>
+  <?php }
+  echo paginate_links();
+?>
+</div>
+
+<?php get_footer();
+
 
 ?>
