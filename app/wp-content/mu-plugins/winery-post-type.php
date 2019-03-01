eebaa379b681c5aa1b985c22688aeee5e446c701
abuna1985 <adamjabundis@gmail.com>
2019-03-01 01:27:22 -0800

    app/wp-content/themes/wrong-turn-winery-theme/front-page.php & /mu-plugins/winery-post-type.php Updated Event Custom Post Type and 'Upcoming Events' section on the Home Page.

--- app/wp-content/mu-plugins/winery-post-type.php
+++ app/wp-content/mu-plugins/winery-post-type.php
@@ -2,9 +2,11 @@
 
 function winery_post_types() {
   register_post_type('event', array(
-    'public'        => true,
-    'show_in_rest'  => true,
-    'labels'        => array(
+    'rewrite'       =>  array('slug' => 'events'),
+    'has_archive'   =>  true,
+    'public'        =>  true,
+    'show_in_rest'  =>  true,
+    'labels'        =>  array(
       'name'          =>  'Events',
       'add_new_item'  =>  'Add New Event',
       'edit_item'     =>  'Edit Event',
