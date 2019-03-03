dd30fecc6b1099ecc43434852289d931f91d2491
abuna1985 <adamjabundis@gmail.com>
2019-03-02 19:58:14 -0800

    app/wp-content/themes/wrong-turn-winery-theme/archive-event.php app/wp-content/themes/wrong-turn-winery-theme/front-page.php app/wp-content/themes/wrong-turn-winery-theme/page-past-events.php Added dynamic Event info for the Front Page, Event Archives, and 'Past Events' Page.

--- app/wp-content/themes/wrong-turn-winery-theme/archive-event.php
+++ app/wp-content/themes/wrong-turn-winery-theme/archive-event.php
@@ -7,28 +7,22 @@ get_header(); ?>
     <div class="page-banner__content container container--narrow">
       <h1 class="page-banner__title">All Events</h1>
       <div class="page-banner__intro">
-        <p>See what is going in our world.</p>
+        <p>See what is going in our winery.</p>
     </div>
   </div>  
 </div>
 
 <div class="container container--narrow page-section">
-<?php 
+<?php
   while(have_posts()) {
-   the_post(); ?> 
-    <div class="event-summary">
-      <a class="event-summary__date t-center" href="#">
-        <span class="event-summary__month">Mar</span>
-        <span class="event-summary__day">25</span>  
-      </a>
-      <div class="event-summary__content">
-        <h5 class="event-summary__title headline headline--tiny"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h5>
-        <p><?php echo wp_trim_words(get_the_content(), 18); ?> <a href="<?php the_permalink(); ?>" class="nu gray">Learn more</a></p>
-      </div>
-    </div>
-  <?php }
-  echo paginate_links();
-?>
+    the_post();
+    get_template_part('template-parts/content-event');
+  } 
+    echo paginate_links();
+  ?>
+  <hr class="section-break">
+  
+  <p>Looking for a recap of past events? <a href="<?php echo site_url('/past-events') ?>">Check out our past events archive</a></p>
 </div>
 
 <?php get_footer();
