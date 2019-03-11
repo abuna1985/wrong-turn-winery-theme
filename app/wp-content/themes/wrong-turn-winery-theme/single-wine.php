d921b604401ba77dbe0c84d0a96ccf929ba50882
abuna1985 <adamjabundis@gmail.com>
2019-03-10 17:35:00 -0700

    Added staff section within the Wine Custom Post.

--- app/wp-content/themes/wrong-turn-winery-theme/single-wine.php
+++ app/wp-content/themes/wrong-turn-winery-theme/single-wine.php
@@ -22,41 +22,67 @@
       <div class="generic-content"><?php the_content(); ?></div>
       <?php 
 
-      $today = date('Ymd');
-      $homepageEvents = new WP_Query(array(
-        'posts_per_page'  => 2,
-        'post_type'       => 'event',
-        'meta_key'        => 'event_date',
-        'orderby'        => 'meta_value_num',
-        'order'           => 'ASC',
-        'meta_query'      => array(
-          array(
-            'key'     => 'event_date',
-            'compare' => '>=',
-            'value'   => $today,
-            'type'    => 'DATE'
-          ),
-          array(
-            'key'     => 'related_wines',
-            'compare' => 'LIKE',
-            'value'   => '"' .get_the_ID() . '"'
+        $relatedStaff = new WP_Query(array(
+          'posts_per_page' => -1,
+          'post_type' => 'staff',
+          'orderby' => 'title',
+          'order' => 'ASC',
+          'meta_query' => array(
+            array(
+              'key' => 'related_wines',
+              'compare' => 'LIKE',
+              'value' => '"' . get_the_ID() . '"'
+            )
           )
-        )
-      ));
+        ));
 
-      if ($homepageEvents->have_posts()) {
-        echo '<hr class="section-break">';
-        echo '<h2 class="headline headline--medium">Events Featuring ' . get_the_title() . '</h2>';
+        if ($relatedStaff->have_posts()) {
+          echo '<hr class="section-break">';
+        echo '<h2 class="headline headline--medium">' . get_the_title() . ' Experts</h2>';
 
-        while($homepageEvents->have_posts()) {
-          $homepageEvents->the_post(); 
-          get_template_part('template-parts/content-event');
+        while($relatedStaff->have_posts()) {
+          $relatedStaff->the_post(); ?>
+          <li><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></li>
+        <?php }
         }
-      }
 
-      wp_reset_postdata();
+        wp_reset_postdata();
 
-    ?>
+        $today = date('Ymd');
+        $homepageEvents = new WP_Query(array(
+          'posts_per_page'  => 2,
+          'post_type'       => 'event',
+          'meta_key'        => 'event_date',
+          'orderby'        => 'meta_value_num',
+          'order'           => 'ASC',
+          'meta_query'      => array(
+            array(
+              'key'     => 'event_date',
+              'compare' => '>=',
+              'value'   => $today,
+              'type'    => 'DATE'
+            ),
+            array(
+              'key'     => 'related_wines',
+              'compare' => 'LIKE',
+              'value'   => '"' .get_the_ID() . '"'
+            )
+          )
+        ));
+
+        if ($homepageEvents->have_posts()) {
+          echo '<hr class="section-break">';
+          echo '<h2 class="headline headline--medium">Events Featuring ' . get_the_title() . '</h2>';
+
+          while($homepageEvents->have_posts()) {
+            $homepageEvents->the_post(); 
+            get_template_part('template-parts/content-event');
+          }
+        }
+
+        wp_reset_postdata();
+
+      ?>
 
     </div>
 
