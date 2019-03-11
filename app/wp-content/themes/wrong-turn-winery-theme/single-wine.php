4b60e7b527a6a68f797509b0a9fa4d8bf75ed435
abuna1985 <adamjabundis@gmail.com>
2019-03-11 01:59:40 -0700

    Implement the pageBanner function within functions.php

--- app/wp-content/themes/wrong-turn-winery-theme/single-wine.php
+++ app/wp-content/themes/wrong-turn-winery-theme/single-wine.php
@@ -3,23 +3,26 @@
   get_header();
 
   while(have_posts()) {
-    the_post(); ?>
-    <div class="page-banner">
-      <div class="page-banner__bg-image" style="background-image: url(<?php echo get_theme_file_uri('/images/bottles.jpg'); ?>);"></div>
-        <div class="page-banner__content container container--narrow">
-          <h1 class="page-banner__title"><?php the_title(); ?></h1>
-          <div class="page-banner__intro">
-            <p>DONT FORGET TO REPLACE ME LATER</p>
-        </div>
-      </div>  
-    </div>
-
+    the_post(); 
+    pageBanner();
+    ?>
+   
     <div class="container container--narrow page-section">
       <div class="metabox metabox--position-up metabox--with-home-link">
         <p><a class="metabox__blog-home-link" href="<?php echo get_post_type_archive_link('wine'); ?>"><i class="fa fa-home" aria-hidden="true"></i> All Wines </a> <span class="metabox__main"><?php the_title(); ?></span></p>
       </div>
 
-      <div class="generic-content"><?php the_content(); ?></div>
+      <div class="generic-content">
+        <div class="row group">
+          <div class="one-third">
+            <?php the_post_thumbnail('staffPortrait'); ?>
+          </div>
+          <div class="two-thirds">
+            <?php the_content(); ?>
+          </div>
+        </div>
+      </div>
+      
       <?php 
 
         $relatedStaff = new WP_Query(array(
@@ -38,12 +41,18 @@
 
         if ($relatedStaff->have_posts()) {
           echo '<hr class="section-break">';
-        echo '<h2 class="headline headline--medium">' . get_the_title() . ' Experts</h2>';
-
-        while($relatedStaff->have_posts()) {
-          $relatedStaff->the_post(); ?>
-          <li><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></li>
-        <?php }
+          echo '<h2 class="headline headline--medium">' . get_the_title() . ' Experts</h2>';
+          echo '<ul class="staff-cards">';
+          while($relatedStaff->have_posts()) {
+            $relatedStaff->the_post(); ?>
+            <li class="staff-card__list-item">
+              <a class="staff-card" href="<?php the_permalink(); ?>">
+                <img class="staff-card__image" src="<?php the_post_thumbnail_url('staffLandscape'); ?>">
+                <span class="staff-card__name"><?php the_title(); ?></span>
+              </a>
+            </li>
+          <?php }
+          echo '</ul>';
         }
 
         wp_reset_postdata();
