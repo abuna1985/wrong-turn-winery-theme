680300eb222146d53aca243ef86e7124f164c06e
abuna1985 <adamjabundis@gmail.com>
2019-03-12 03:55:22 -0700

    Added the Location Post Type

--- app/wp-content/mu-plugins/winery-post-type.php
+++ app/wp-content/mu-plugins/winery-post-type.php
@@ -1,6 +1,24 @@
 <?php 
 
 function winery_post_types() {
+  // Location Post Type
+  register_post_type('location', array(
+    'supports'      =>  array('title', 'editor', 'excerpt'),
+    'rewrite'       =>  array('slug' => 'locations'),
+    'has_archive'   =>  true,
+    'public'        =>  true,
+    'show_in_rest'  =>  true,
+    'labels'        =>  array(
+      'name'            =>  'Locations',
+      'add_new_item'    =>  'Add New Location',
+      'edit_item'       =>  'Edit Location',
+      'all_items'       =>  'All Locations',
+      'view_item'       =>  'View Location',
+      'singular_name'   =>  'Location'
+    ),
+    'menu_icon' => 'dashicons-location-alt'
+  ));
+  
   // Event Post Type
   register_post_type('event', array(
     'supports'      =>  array('title', 'editor', 'excerpt'),
