2dcac6100c2561e94c379fa291a9e8b02ce8a5af
abuna1985 <adamjabundis@gmail.com>
2019-03-25 23:35:31 -0700

    Add files from the includes folder.

--- app/wp-content/themes/wrong-turn-winery-theme/functions.php
+++ app/wp-content/themes/wrong-turn-winery-theme/functions.php
@@ -7,3 +7,8 @@ require get_theme_file_path('/includes/winery-files.php');
 require get_theme_file_path('/includes/winery-features.php');
 require get_theme_file_path('/includes/winery-adjust-queries.php');
 require get_theme_file_path('/includes/google-map-api.php');
+require get_theme_file_path('/includes/redirect-subscribers.php');
+require get_theme_file_path('/includes/remove-sub-admin-bar.php');
+require get_theme_file_path('/includes/custom-header-url.php');
+require get_theme_file_path('/includes/winery-login-css.php');
+require get_theme_file_path('/includes/winery-login-title.php');
