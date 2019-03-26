b873b90e9b92fb6e5bc7b55a7cddbaeab022eaf0
abuna1985 <adamjabundis@gmail.com>
2019-03-25 18:36:05 -0700

    Added template parts and logic for fallback PHP search

---
+++ app/wp-content/themes/wrong-turn-winery-theme/template-parts/content-staff.php
@@ -0,0 +1,8 @@
+<div class="post-item">
+  <li class="staff-card__list-item">
+    <a class="staff-card" href="<?php the_permalink(); ?>">
+      <img class="staff-card__image" src="<?php the_post_thumbnail_url('staffLandscape'); ?>">
+      <span class="staff-card__name"><?php the_title(); ?></span>
+    </a>
+  </li>
+</div>
